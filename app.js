f045feafcf29a79944e4a173101e8b435940d937
Oyatulloh-Isroilov <oyatullohisroilov5@gmail.com>
2023-10-20 20:54:09 +0500

    4-oy 7-dars

---
+++ app.js
@@ -0,0 +1,26 @@
+// 2-masala
+
+// function SezarCode(arr, arr2) {
+//   let arrr = prompt("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z");
+//   callback();
+//   console.log(arr);
+// }
+
+// function shrift1() {
+//   arr.splice("d,e,f,g,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,a,b,c");
+//   console.log(shrift1);
+// }
+
+//
+// 3-masala
+function add(arr1, callback) {
+    console.log(a + b);
+    callback();
+  }
+  
+  function sss() {
+    console.log("callback function ichi");
+  }
+  
+  add(4, 5, sss);
+  // oxshamagan proekt
